a616aea4c8453b2bf39ebd56260b6823260fb96b
Matheus Queiroz <m.dimello@hotmail.com>
2016-09-11 19:44:03 -0300

    - Correcoes gerais para entregar

--- TRAB1-1/TestaLista.bat
+++ TRAB1-1/TestaLista.bat
@@ -2,4 +2,4 @@ REM executa os testes relativos ao modulo lista
 
 del "estatisticas.estat"
 
-"TRAB1-1.exe" /s"TesteLista"  /l"TesteLista"   /a"estatisticas"
+TRAB1-1.exe /sTesteLista.script > TesteLista.txt
