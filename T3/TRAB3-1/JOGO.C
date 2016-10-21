e8a555ced7415fafee5333f576a0b6c9b0414873
Felipe Pessoa <felipessoaf@hotmail.com>
2016-10-21 16:41:20 -0200

    criando arquivos no padrão e pastas novas do t3

---
+++ T3/TRAB3-1/JOGO.C
@@ -0,0 +1,27 @@
+/***************************************************************************
+*  $MCI Módulo de implementação: JOGO	Jogo de xadrez
+*
+*  Arquivo gerado:              JOGO.c
+*  Letras identificadoras:      JOGO
+*
+*  Nome da base de software:    Arcabouço para a automação de testes de programas redigidos em C
+*
+*  Projeto: INF 1301 / Juiz de Xadrez
+*  -------------------------------------------Gestor:  LES/DI/PUC-Rio
+*  Autores: pf - Pedro Ferreira
+*           mmq - Matheus de Mello Queiroz
+*			fpf - Felipe Pessoa de Freitas	
+*
+*  $HA Histórico de evolução:
+*     Versão  Autor     Data       Observações
+*     1       fpf     21/out/2016  início da implementação
+*
+***************************************************************************/
+
+#define JOGO_OWN
+#include "JOGO.h"
+#undef JOGO_OWN
+
+/*****  Código das funções exportadas pelo módulo  *****/
+
+/********** Fim do módulo de implementação: JOGO  Jogo de xadrez **********/
---
+++ T3/TRAB3-1/TESTJOGO.C
@@ -0,0 +1,60 @@
+/***************************************************************************
+*  $MCI Módulo de implementação: TJOGO Teste jogo de xadrez
+*
+*  Arquivo gerado:              TestJOGO.c
+*  Letras identificadoras:      TJOGO
+*
+*  Nome da base de software:    Arcabouço para a automação de testes de programas redigidos em C
+*
+*  Projeto: INF 1301 / Juiz de Xadrez
+*  -------------------------------------------Gestor:  LES/DI/PUC-Rio
+*  Autores: pf - Pedro Ferreira
+*           mmq - Matheus de Mello Queiroz
+*			fpf - Felipe Pessoa de Freitas
+*
+*  $HA Histórico de evolução:
+*     Versão  Autor      Data     Observações
+*     1        fpf   21/out/2016  início do desenvolvimento
+*
+***************************************************************************/
+
+#include    "TST_Espc.h"
+
+#include    "Generico.h"
+#include    "LerParm.h"
+
+#include    "JOGO.H"
+
+
+/*****  Código das funções exportadas pelo módulo  *****/
+
+
+/***********************************************************************
+*
+*  $FC Função: TJOGO &Testar jogo
+*
+*  $ED Descrição da função
+*     Testa a execução de um jogo de xadrez
+*
+*     Comandos disponíveis:
+*
+*
+***********************************************************************/
+
+TST_tpCondRet TST_EfetuarComando( char * ComandoTeste )
+{
+	int numLidos   = -1 ,
+		CondRetEsp = -1  ;
+
+	TST_tpCondRet CondRet = TST_CondRetOK;
+
+	
+		
+
+	return TST_CondRetNaoConhec ;
+} /* Fim função: TLIS &Testar lista */
+
+
+
+/********** Fim do módulo de implementação: TTAB Teste tabuleiro de xadrez **********/
+
